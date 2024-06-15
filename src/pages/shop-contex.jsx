f5472202945f06f01b774c1fe4779a7cd3fafb5f
mohamed <mehdaoui200309@gmail.com>
2024-06-15 17:37:34 +0100

    dirst commit

--- src/pages/shop-contex.jsx
+++ src/pages/shop-contex.jsx
@@ -1,30 +1,31 @@
 import React, { createContext, useState } from "react";
 import { PROD } from "../list";
 
-export const shopcontext =createContext(null);
+export const shopcontext = createContext(null);
 
 
 
 export const ShopContextProvider = (props) => {
-    const [cartitem, setCarItems]= useState({0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0});
+    const [cartitem, setCarItems] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 });
 
     const addToCart = (pid) => {
-        setCarItems( (prev) => ({...prev, [pid]: prev[pid]+1}));
+        setCarItems((prev) => ({ ...prev, [pid]: prev[pid] + 1 }));
     };
     const getTotal = () => {
-        let total =0;
-        for (const item in  cartitem) {
-            if (cartitem[item] > 0){
-                let k=PROD.find((product) => product.id === Number(item));
-                total += cartitem[item]*k.price;
+        let total = 0;
+        for (const item in cartitem) {
+            if (cartitem[item] > 0) {
+                let k = PROD.find((product) => product.id === Number(item));
+                total += cartitem[item] * k.price;
             }
         }
-    ;}
+        return total;
+    }
 
     const removFormCart = (pid) => {
-        setCarItems( (prev) => ({...prev, [pid]: prev[pid]-1}));
+        setCarItems((prev) => ({ ...prev, [pid]: prev[pid] - 1 }));
     };
-    const contextValue = {cartitem,addToCart,removFormCart};
+    const contextValue = { cartitem, addToCart, removFormCart, getTotal };
 
     return <shopcontext.Provider value={contextValue}> {props.children} </shopcontext.Provider>
 };
